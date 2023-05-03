fb303ce87de1618f9d270d8e960ad65be7ea0349
임대현 <eogus1643@gmail.com>
2023-05-03 17:37:58 +0900

    1122

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,6 +3,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
   devServer: {
-    proxy : 'http://localhost:8070/'
+    // proxy : 'http://localhost:8070/'
+    proxy : 'https://port-0-sample-project-e9btb72mlh6e1vvi.sel4.cloudtype.app/'
   }
+  
 })
+
